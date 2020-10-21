1d8597d53e5f996f9cd337022870a3f952ff5057
rasm47 <16821766+rasm47@users.noreply.github.com>
2020-10-21 13:46:00 +0300

    Add cors and helmet middlewares

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
 app.use('/', routes);
 app.listen(port);
--- routes.js
+++ routes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const greeterRouter = require('./routes/greeter'); 
 
 const routes = express.Router()
-    .use('/api/greeter/', greeterRouter);
+    .use('/api/greeter', greeterRouter);
 
 module.exports = routes;
